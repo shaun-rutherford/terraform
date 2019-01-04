113f9b64e0e167e7a8e08efa59c0dcbcaff162e7
Shaun Rutherford <srutherford@blizzard.com>
2019-01-04 09:06:54 -0800

    init commit

---
+++ aws/production/modules/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "The aws region you want to build in"
+}
+
+variable "aws_access_key" {
+    description = "Your aws access key, https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html"
+}
+
+variable "aws_secret_key" {
+    description = "Your aws secret key, https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html"
+}
