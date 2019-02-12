babb4abf690395be8e1477358ef46b075d0e66df
Shaun Rutherford <aeonsies@gmail.com>
2019-02-11 19:12:09 -0800

    init commit variables.tf global

---
+++ aws/Production/us-east-1/Global/variables.tf
@@ -0,0 +1,14 @@
+variable global_region {
+    description = "AWS region to use"
+    type        = "string"
+}
+
+variable global_access_key {
+    description = "AWS access key"
+    type        = "string"
+}
+
+variable global_secret_key {
+    description = "AWS secret key"
+    type        = "string"
+}
