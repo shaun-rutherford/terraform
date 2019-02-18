2dd3474d5f3bf6e30a726e9b49dca2f0d27794a5
Shaun Rutherford <srutherford@blizzard.com>
2019-02-18 10:43:51 -0800

    Adding Production us-east-1 global variables.tf

---
+++ aws/Production/us-east-1/Global/variables.tf
@@ -0,0 +1,15 @@
+variable global_region {
+    description = "AWS region to use"
+    type        = "string"
+    default     = "us-east-1"
+}
+
+variable global_access_key {
+    description = "AWS access key"
+    type        = "string"
+}
+
+variable global_secret_key {
+    description = "AWS secret key"
+    type        = "string"
+}
