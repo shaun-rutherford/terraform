9856e470c322f9e18c8e13a7a16606a2c797e6d2
Shaun Rutherford <srutherford@blizzard.com>
2019-02-18 10:42:00 -0800

    upper casing Modules directory.  Seperating Modules directory into subdirectories to resolve issue with all variables having to be assigned if the module is called, even if you don't intend on using it

---
+++ aws/Modules/provider/variables.tf
@@ -0,0 +1,12 @@
+variable region {
+    description = "AWS region to use"
+    type        = "string"
+}
+variable access_key {
+    description = "AWS access key"
+    type        = "string"
+}
+variable secret_key {
+    description = "AWS secret key"
+    type        = "string"
+}
