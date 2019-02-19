73c3d12f9c646964f697b6e3f209e19c8c77cccc
Shaun Rutherford <aeonsies@gmail.com>
2019-02-18 17:24:14 -0800

    removing uncessary varibles from ec2 variables.tf

--- aws/Production/us-east-1/Ec2/variables.tf
+++ aws/Production/us-east-1/Ec2/variables.tf
@@ -1,23 +1,15 @@
-variable ec2_public_key {
-    description = "AWS ssh public key to upload"
-    type        = "string" 
-}
-
-variable ec2_region {
+variable region {
     description = "AWS region to use"
     type        = "string"
+    default     = "us-east-1"
 }
 
-variable ec2_access_key {
-    description = "AWS access key"
-    type        = "string"
-}
-
-variable ec2_secret_key {
-    description = "AWS secret key"
-    type        = "string"
-}
 variable ec2_key_name {
     description = "Name of your AWS key"
     type        = "string"
+}
+
+variable ec2_public_key {
+    description = "AWS ssh public key to upload"
+    type        = "string" 
 }
