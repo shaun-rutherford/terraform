236fa888f89791b814f6ba24e65535806704956f
Shaun Rutherford <aeonsies@gmail.com>
2019-02-18 17:23:23 -0800

    removing uncessary variables from ec2 main.tf

--- aws/Production/us-east-1/Ec2/main.tf
+++ aws/Production/us-east-1/Ec2/main.tf
@@ -1,8 +1,9 @@
-module "ec2_variables" {
-    source = "../../../Modules"
+provider "aws" {
+  region     = "${var.region}"
+}
+
+module "ec2_keys" {
+    source     = "../../../Modules/key_pair"
     public_key = "${var.ec2_public_key}"
-    region     = "${var.ec2_region}"
-    access_key = "${var.ec2_access_key}"
-    secret_key = "${var.ec2_secret_key}"
     key_name   = "${var.ec2_key_name}"
 }
