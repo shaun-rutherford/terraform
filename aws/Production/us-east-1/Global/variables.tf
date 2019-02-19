3192a8c6f6f72c98b3a93932194a4dc6cd5624d7
Shaun Rutherford <aeonsies@gmail.com>
2019-02-18 16:43:52 -0800

    updateing global variables.tf to standard variable names

--- aws/Production/us-east-1/Global/variables.tf
+++ aws/Production/us-east-1/Global/variables.tf
@@ -1,15 +1,15 @@
-variable global_region {
+variable region {
     description = "AWS region to use"
     type        = "string"
     default     = "us-east-1"
 }
 
-variable global_access_key {
+variable access_key {
     description = "AWS access key"
     type        = "string"
 }
 
-variable global_secret_key {
+variable secret_key {
     description = "AWS secret key"
     type        = "string"
 }
