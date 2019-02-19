087bfd9bea588e1b73284faa60cadf0c9b825aee
Shaun Rutherford <aeonsies@gmail.com>
2019-02-18 17:23:48 -0800

    changing bucket and dynamodb_table to be more explicite in naming convertion

--- aws/Production/us-east-1/Ec2/output.tf
+++ aws/Production/us-east-1/Ec2/output.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt         = true
-    bucket          = "ojutai_terraform_state"
-    dynamodb_table  = "ojutai_terraform_state-lock"
+    bucket          = "ojutai-us-east-1-production-terraform-state"
+    dynamodb_table  = "ojutai_us_east_1_production_terraform_state_lock"
     region          = "us-east-1"
-    key             = "global/s3/terraform.tfstate"
+    key             = "terraform.tfstate"
   }
 }
