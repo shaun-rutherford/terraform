de38fc3905e6b82f2bf22d6014bf47ba98b0a755
Shaun Rutherford <aeonsies@gmail.com>
2019-02-23 12:22:14 -0800

    changing ec2_keys to username_keys

--- aws/Production/us-east-1/Ec2/main.tf
+++ aws/Production/us-east-1/Ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-module "ec2_keys" {
+module "srutherford_keys" {
     source     = "../../../Modules/key_pair"
     public_key = "${var.ec2_public_key[0]}"
     key_name   = "${var.ec2_key_name[0]}"
