0d54f4c30dcd734ae5efff6d968db01db2b73501
Shaun Rutherford <srutherford@blizzard.com>
2019-03-10 15:29:57 -0700

    FIX: updating directory names to upper case

---
+++ aws/Modules/Key_Pair/variables.tf
@@ -0,0 +1,9 @@
+variable public_key {
+    description = "AWS ssh public key to upload"
+    type        = "string" 
+}
+
+variable key_name {
+    description = "Name of your AWS key"
+    type        = "string"
+}
---
+++ aws/Modules/VPC/variables.tf
@@ -0,0 +1,14 @@
+variable cidr_block {
+    description = "IP Cidr block for subnet"
+    type        = "string"
+}
+
+variable vpc_name {
+    description = "Name of the VPC Subnet"
+    type        = "string"
+}
+
+variable instance_tenancy {
+    description = "A tenancy option for instances launched into the VPC"
+    type        = "string"
+}
