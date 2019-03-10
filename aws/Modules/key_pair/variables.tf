fd63df33979cde9e9fd41e93099afccd460b20c1
Shaun Rutherford <srutherford@blizzard.com>
2019-03-10 15:32:12 -0700

    FIX: deleting underscore directory names

--- aws/Modules/key_pair/variables.tf
+++
@@ -1,9 +0,0 @@
-variable public_key {
-    description = "AWS ssh public key to upload"
-    type        = "string" 
-}
-
-variable key_name {
-    description = "Name of your AWS key"
-    type        = "string"
-}
