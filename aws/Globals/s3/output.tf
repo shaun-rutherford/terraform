388bcbfee817eff76d0892b27487214559d42ae9
Shaun Rutherford <srutherford@blizzard.com>
2019-03-12 16:31:56 -0700

    FIX: Removing top level Globals directory, no longer using.  Instead putting Globals under each availability zone

--- aws/Globals/s3/output.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt         = true
-    bucket          = "ojutai_terraform_state"
-    dynamodb_table  = "ojutai_terraform_state-lock"
-    region          = "us-east-1"
-    key             = "global/s3/terraform.tfstate"
-  }
-}
--- aws/Globals/s3/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_access_key" {
-    description = "Your aws access key, https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html"
-}
-
-variable "aws_secret_key" {
-    description = "Your aws secret key, https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html"
-}
